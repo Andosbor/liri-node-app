28d801632321efa6c7913ae64a59e4ceb8601e0b
AndrewO <andrewosborn101@gmail.com>
2019-03-29 15:59:53 -0500

    finished spotify

--- liri.js
+++ liri.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
+
 var keys = require("./keys.js");
 
 var axios = require("axios");
+
 var moment = require('moment');
 moment().format();
 
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+var fs = require("fs")
 
+
+/*
 axios
   .get("https://en.wikipedia.org/wiki/Kudos_(granola_bar)")
   .then(function(response) {
@@ -15,13 +18,27 @@ axios
     // Then log the body from the site!
     console.log(response.data);
   })
-  
-
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+  */
+ 
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+ 
+  if (process.argv[2] === "spotify-this-song"){
+    var songName = process.argv.slice(3).join(" ");
+    if (songName === ""){
+      songName = "The Sign Ace of Base";
     }
-   
-  console.log(data); 
-  });
-  //if (process.argv[2] === "concert-this")
+    spotify
+      .search({ type: 'track', query: songName })
+      .then(function(response){
+        //console.log(response.tracks.items[0])
+        console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
+        console.log("Name: " + response.tracks.items[0].name);
+        console.log("Song Preview: " + response.tracks.items[0].preview_url);
+        console.log("Album: " + response.tracks.items[0].album.name);
+      })
+      .catch(function(err) {
+        console.log(err); 
+      });
+
+  }
